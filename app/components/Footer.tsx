b7e269067cc903719ec6c18abc4389b199160aa7
Harsh Tiwari <harshtiwari@Harshs-MacBook-Air.local>
2023-11-30 00:39:59 +0530

    check

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <section className="flex justify-center items-center bg-black h-[7vh]">
-      <div>Footer</div>
+    <section className="flex justify-center items-center bg-black h-[10vh]">
+      <h1 className="text-white">&copy;Copyright 2023 HarshWebDev. All rights reserved.</h1>
     </section>
   );
 };
