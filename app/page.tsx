d6888b44315df7123320d95aff4dd42901a42e95
Harsh Tiwari <harshtiwari@Harshs-MacBook-Air.local>
2023-11-30 00:32:02 +0530

    check

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,10 @@
-import Image from "next/image";
+
 import Navbar from "./components/Navbar";
-import HeroSection from "./components/HeroSection";
-import About from "./components/About";
-import Project from "./components/Project";
-import Connect from "./components/Connect";
-import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
-      {/* <Project />
-      <About />
-      <Connect />
-      <Footer/> */}
     </main>
   );
 }
