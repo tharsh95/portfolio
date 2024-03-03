a6d638e0c331980bec4908cb34eb467ac16e7c88
Harsh Tiwari <harshtiwari@Harshs-MacBook-Air.local>
2024-03-03 13:34:56 +0530

    made responsive

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const About = () => {
   return (
-    <section className=" relative flex justify-around items-center bg-[#111827] lg:p-10 md:p-10 p-12 sm:p-12 lg:h-[70vh] md:h-[60vh] h-[85vh] sm:h-[85vh]">
+    <section className=" relative flex justify-around items-center bg-[#111827] lg:p-10 md:p-10 p-12 sm:p-12">
       <div className="lg:w-1/2 md:w-3/4 sm:w-full">
         <h1 className="text-white text-4xl font-bold text-center ">About me</h1>
         <p className="mt-5 mb-3 text-gray-300 text-2xl">
